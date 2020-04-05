f9e92dbcbdeaa550139bec4b2d29f0feacfbf751
Rahul Das <rahuldas@LPMB-Kol-Priyanjit.local>
2020-04-05 21:58:12 +0530

    kingdom meta data generated and further breaking down into sub modules

--- geektrust.js
+++ geektrust.js
@@ -2,8 +2,11 @@
 const fileName = process.argv[2];
 const readFile = require('./modules/file-reader');
 const inputGenerator = require('./modules/input-generator');
+const kingdomMetaDataGenerator = require('./modules/kingdom-meta-data');
 
 // read data from the input file
 let data = readFile(fileName).toString();
+// generate the kingdoms meta data
+let kingdomMetaData = kingdomMetaDataGenerator();
 // parse the input
-let parsedInput = inputGenerator(data);
+let parsedInput = inputGenerator(data, kingdomMetaData);
--- modules/input-generator.js
+++ modules/input-generator.js
@@ -1,33 +1,19 @@
-/**
- * Functio that counts and returns the number of occurances of all the characters in an array
- * @param {Array} charArr array of chartacters
- * @return {Object} the number of occurances of each character
- */
-function charCounter(charArr){
-    let returnObj = {},
-    uppercaseChar;
-    for (let i = 0; i < charArr.length; i++){
-        uppercaseChar = charArr[i].toUpperCase();
-        if (returnObj.hasOwnProperty(uppercaseChar)){
-            returnObj[uppercaseChar]++;
-        } else{
-            returnObj[uppercaseChar] = 1;
-        }
-    }
-    return returnObj;
-}
 
+const charCounter = require('./char-counter'),
+ decoder = require('./decoder');
 /**
  * Function that takes in the file input and returns the parsed input object
  * @param {String} rawInput the content of the file
+ * @param {Object} kingdomMetaData meta data of all the kingdoms 
  * @returns {Object} parsed input object
  */
-const inputGenerator = function(rawInput){
+const inputGenerator = function(rawInput, kingdomMetaData){
     return rawInput.split('\n').map(inputLine=>{
-        let message = inputLine.split(' ');
+        let message = inputLine.split(' '),
+        kingdomNameInUpperCase = message[0].toUpperCase();
         return{
-            kingdom: message[0].toUpperCase(),
-            letter_count: charCounter(message[1].split(''))
+            kingdom: kingdomNameInUpperCase,
+            letter_count: charCounter(message[1].split(''), decoder, kingdomMetaData[kingdomNameInUpperCase].offset)
         }
     })
 }
